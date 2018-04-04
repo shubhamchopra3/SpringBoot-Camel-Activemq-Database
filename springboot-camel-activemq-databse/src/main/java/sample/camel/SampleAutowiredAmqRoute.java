ec359c4552603792e930606756da7eccd6bd80fe
Shubham Chopra <shubhamchopra553@gmail.com>
2018-04-04 15:55:53 +0530

    Update SampleAutowiredAmqRoute.java

--- springboot-camel-activemq-databse/src/main/java/sample/camel/SampleAutowiredAmqRoute.java
+++ springboot-camel-activemq-databse/src/main/java/sample/camel/SampleAutowiredAmqRoute.java
@@ -7,16 +7,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleAutowiredAmqRoute extends RouteBuilder {
 
-	String msg="Hello from ofss";
-	String output;
+	//String msg="Hello from ofss";
+	 String msg="{\"id\":\"4\",\"item\":\"GOT\",\"description\":\"hello\"}";
+	
     @Override
     public void configure() throws Exception {
+	    //this is used to recieve data from activemq
         from("activemq:sample.queue")
             .to("bean:databaseService?method=printjson(${body})");
 
-       /* from("timer://foo?repeatCount=1")     // use from("timer://foo?repeatCount=1")  to send only once the route starts and use  from("timer:bar") to send every second
+	    /* This is used to send data to activemq*/
+        from("timer://foo?repeatCount=1")     // use from("timer://foo?repeatCount=1")  to send only once the route starts and use  from("timer:bar") to send every second
         .setBody(constant(msg))
-        .to("activemq:sample.queue");  */   //sample.queue is the name of queue
+        .to("activemq:sample.queue");     //sample.queue is the name of queue
     }
     
    
